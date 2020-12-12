188011c97165b54d56ea38d79f36e70039f96bd3
Samu Linden <samu.linden@gmail.com>
2020-12-12 12:00:15 +0100

    Adds starting point files

---
+++ 2020/startingPoint/index.js
@@ -0,0 +1,12 @@
+const solve1 = (input) => {};
+
+const solve2 = (input) => {};
+
+const input = ``;
+
+const inputExample = ``;
+
+solve1(inputExample);
+// solve1(input)
+// solve2(inputExample)
+// solve2(input)
